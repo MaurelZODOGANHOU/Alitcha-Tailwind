9521bbbee909159acbd4a96ca467aef6fa803d0b
Maurel ZODOGANHOU <maurelcath24@gmail.com>
2023-01-26 23:55:01 +0100

    Bouton 2

---
+++ src/components/button/button2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Bouton2 = () => {
+  return (
+    <div class="flex space-x-2 justify-center ">
+
+        <button type="button" class="text-decoration-color:black rounded animate-pulse flex space-x-4 bg-orange-500 opacity-1500 ">
+            BOUTON2
+        </button>
+    
+</div>
+  )
+}
+
+export default Bouton2
