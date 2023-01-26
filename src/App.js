5e18bdfcfaa1170ec456a3cb989bb1707a513441
Maurel ZODOGANHOU <maurelcath24@gmail.com>
2023-01-26 22:51:43 +0100

    Création du premier bouton. Il a différentes couleurs au hover, focus. Il s'anime comme un bounce .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import Bouton from "./components/button/button";
+import React, { useState } from "react";
 import "./index.css"
 
 function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    /*<div className="w-screen h-screen flex items-center justify-center">
       <div className="text-3xl text-orange-600">
         Welcome on Alitcha Tailwind
       </div>
-    </div>
+    </div>*/
+    <Bouton/>
   );
 }
 
---
+++ src/components/button/button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Bouton = () => {
+  return (
+    <div class="flex space-x-2 justify-center">
+
+        <button type="button" class=" text-decoration-color : bg-orange-600 hover:bg-sky-700 active:bg-green-500 animate-bounce  rounded">
+            BOUTON1
+        </button>
+    
+</div>
+  )
+}
+
+export default Bouton
